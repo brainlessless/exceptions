902f81a1c08cc159c47a0ebc1e6217d890e214f4
brainless <maggot.vitaly@gmail.com>
2023-04-14 17:38:50 +0300

    push ubuntu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    public static int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+    public static int getByIndex(int index) {
+        if (index >= 0 && index < arr.length)
+            return arr[index];
+        return -1;
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, Ubuntu!");
+        System.out.println(getByIndex(100));
     }
 }
